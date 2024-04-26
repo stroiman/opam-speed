e2106107d82b2af19961cdc952d696a7ecf07f7f
Peter Strøiman <peter@stroiman.com>
2024-04-26 22:41:59 +0200

    We now became self-testing.

--- lib/Runner.ml
+++ lib/Runner.ml
@@ -2,7 +2,7 @@ open Domain
 
 type test_result = { success : bool }
 
-let run suite =
+let run_suite suite =
   try
     suite.examples |> List.iter (fun x -> x.f ());
 
@@ -10,3 +10,10 @@ let run suite =
   with _ -> { success = false }
 
 let is_success { success } = success
+
+(** This runs the test suite and exits the program. If the test suite is 
+  successful, it will exit with exit code zero, otherwise it will exit with 
+  exit code 1.
+*)
+let run_main suite =
+  match is_success @@ run_suite suite with true -> exit 0 | false -> exit 1
--- test/test_speed.ml
+++ test/test_speed.ml
@@ -8,13 +8,32 @@ let passing_test () = ()
 let failing_test () = raise TestError
 
 let () =
-  let first_result =
-    { examples = [ { name = "Passing example"; f = passing_test } ] }
-    |> run |> is_success
-  in
-  expect first_result be_true;
-  let second_result =
-    { examples = [ { name = "Failing example"; f = failing_test } ] }
-    |> run |> is_success
-  in
-  expect second_result be_false
+  {
+    examples =
+      [
+        {
+          name =
+            "is_success should return success=true when test case doesn't raise";
+          f =
+            (fun _ ->
+              expect
+                ({
+                   examples = [ { name = "Passing example"; f = passing_test } ];
+                 }
+                |> run_suite |> is_success)
+                be_true);
+        };
+        {
+          name = "Run should return success=false when test case raises";
+          f =
+            (fun _ ->
+              expect
+                ({
+                   examples = [ { name = "Failing example"; f = failing_test } ];
+                 }
+                |> run_suite |> is_success)
+                be_false);
+        };
+      ];
+  }
+  |> run_main
