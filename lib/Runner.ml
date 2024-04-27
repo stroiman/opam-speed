8f9ba1ffab44c6525431ce0d4643604135883484
Peter Strøiman <peter@stroiman.com>
2024-04-28 01:57:35 +0200

    Print example names while testing

--- lib/Runner.ml
+++ lib/Runner.ml
@@ -2,9 +2,40 @@ open Domain
 
 type test_result = { success: bool }
 
+let unwind ?on_error ?on_success ~(protect : 'a -> unit) f x =
+  try
+    let y = f x in
+    (match on_success with
+     | Some x -> x ()
+     | None -> ());
+    protect x;
+    y
+  with
+  | e ->
+    (match on_error with
+     | Some x -> x ()
+     | None -> ());
+    protect x;
+    raise e
+;;
+
+let run_example example =
+  Format.open_vbox 2;
+  Format.print_string example.name;
+  Format.print_cut ();
+  unwind
+    ~on_error:(fun _ -> Format.print_string "Failure")
+    ~on_success:(fun _ -> Format.print_string "Success")
+    ~protect:(fun _ ->
+      Format.close_box ();
+      Format.print_cut ())
+    example.f
+    ()
+;;
+
 let run_suite suite =
   try
-    suite.examples |> List.iter (fun x -> x.f ());
+    suite.examples |> List.iter run_example;
     { success= true }
   with
   | _ -> { success= false }
