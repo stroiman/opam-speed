056ec25ab351576621bbb0b689db8549bdcce685
Peter Strøiman <peter@stroiman.com>
2024-05-02 18:02:00 +0200

    Don't throw in ppx_deriver on non-record types
    Didn't work correctly with ppx_import, which is hugely usefule for this
    purpose as it helps keep test code separate from production code.
    
    There should be a way to do it still by detecting the compilation cycle,
    but I was unable to get that to compile, and the error is just a minor
    thing; whoever is using it should understand the purpose. Now it just
    doesn't generate any code if not used on a record.
    
    Solution: https://tarides.com/blog/2019-05-09-an-introduction-to-ocaml-ppx-ecosystem/#compatibility-with-ppx_import

--- match_generator/ppx_matcher.ml
+++ match_generator/ppx_matcher.ml
@@ -6,47 +6,51 @@ let str_gen ~loc ~path:_ (_rec, t) =
   let (module Ast) = Ast_builder.make loc in
   let fields =
     match t.ptype_kind with
-    | Ptype_record fields -> fields
-    | _ -> Location.raise_errorf ~loc "Matcher only works on records"
+    | Ptype_record fields -> Some fields
+    | _ -> None
+    (* Location.raise_errorf ~loc "Matcher only works on records" *)
   in
-  let lident_of_field field =
-    Ast_builder.Default.Located.lident ~loc:field.pld_name.loc
-      field.pld_name.txt
-  in
-  let pat =
-    let type_name = t.ptype_name.txt in
-    Ast.pvar (type_name ^ "_matcher")
-  in
-  let expr =
-    let body =
-      let init = [%expr Ok ()] in
+  match fields with
+  | None -> []
+  | Some fields ->
+    let lident_of_field field =
+      Ast_builder.Default.Located.lident ~loc:field.pld_name.loc
+        field.pld_name.txt
+    in
+    let pat =
+      let type_name = t.ptype_name.txt in
+      Ast.pvar (type_name ^ "_matcher")
+    in
+    let expr =
+      let body =
+        let init = [%expr Ok ()] in
+        fields
+        |> List.fold_right ~init ~f:(fun field iter ->
+          let lident_actual = Loc.make ~loc (Longident.parse "actual") in
+          [%expr
+            [%e iter]
+            |> Speed.Assertions.AssertionResult.bind ~f:(fun _ ->
+              match [%e Ast.pexp_ident (lident_of_field field)] with
+              | None -> Ok ()
+              | Some v ->
+                [%e
+                  Ast.pexp_field
+                    (Ast.pexp_ident lident_actual)
+                    (lident_of_field field)]
+                |> v
+                |> Speed.Assertions.AssertionResult.map ignore
+            )]
+        )
+      in
+      let init = [%expr fun actual -> [%e body]] in
       fields
-      |> List.fold_right ~init ~f:(fun field iter ->
-        let lident_actual = Loc.make ~loc (Longident.parse "actual") in
-        [%expr
-          [%e iter]
-          |> Speed.Assertions.AssertionResult.bind ~f:(fun _ ->
-            match [%e Ast.pexp_ident (lident_of_field field)] with
-            | None -> Ok ()
-            | Some v ->
-              [%e
-                Ast.pexp_field
-                  (Ast.pexp_ident lident_actual)
-                  (lident_of_field field)]
-              |> v
-              |> Speed.Assertions.AssertionResult.map ignore
-          )]
+      |> List.fold_right ~init ~f:(fun name inner ->
+        Ast.pexp_fun (Optional name.pld_name.txt) None
+          (Ast.ppat_var name.pld_name)
+          inner
       )
     in
-    let init = [%expr fun actual -> [%e body]] in
-    fields
-    |> List.fold_right ~init ~f:(fun name inner ->
-      Ast.pexp_fun (Optional name.pld_name.txt) None
-        (Ast.ppat_var name.pld_name)
-        inner
-    )
-  in
-  [[%stri let [%p pat] = [%e expr]]]
+    [[%stri let [%p pat] = [%e expr]]]
 ;;
 
 let str_type_decl = Deriving.Generator.make_noarg str_gen
