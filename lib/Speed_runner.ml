60418b2087ac0e23c04fd4cd5f29071ef726a411
Peter Strøiman <peter@stroiman.com>
2024-05-24 20:19:46 +0200

    Remove unused case

--- lib/Speed_runner.ml
+++ lib/Speed_runner.ml
@@ -86,7 +86,6 @@ let add_pp ?(no_break = false) pp ctx =
 module ExampleRunner = struct
   type test_outcome =
     | Success
-    | Failure
     | FailureWithFormat of (Format.formatter -> unit)
 
   module type EXAMPLE_RUNNER = sig
@@ -161,13 +160,6 @@ module Reporter = struct
         empty_suite_result with
         no_of_passing_examples= empty_suite_result.no_of_passing_examples + 1;
         pp= Some (Format.dprintf "@{<green>✔@} %s" name);
-      }
-    | Failure ->
-      {
-        empty_suite_result with
-        success= false;
-        no_of_failing_examples= empty_suite_result.no_of_failing_examples + 1;
-        pp= Some (Format.dprintf "@{<red>✘@} %s" name);
       }
     | FailureWithFormat pp ->
       {
